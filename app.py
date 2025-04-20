6ae65d8cf4bcb858b198f73c59e8814ebbd7c346
Ashish Singh <rajs02073@gmail.com>
2025-04-20 21:18:40 +0530

    comment main in app.py

--- app.py
+++ app.py
@@ -49,6 +49,6 @@ async def health_check():
 
 asgi_app = app
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000) 
